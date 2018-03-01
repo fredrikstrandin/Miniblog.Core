eaa838dbd10f6ecd368dbc677bb1a1fbbf494476
Fredrik Strandin <strandin72@gmail.com>
2018-03-01 19:23:02 +0100

    Moving project and rename directory

--- src/Multiblog.Core/Attribute/TenantAttribute.cs
+++ src/Multiblog.Core/Attribute/TenantAttribute.cs
@@ -1,12 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.Extensions.Options;
-using MongoDB.Driver;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Multiblog.Core.Model.Setting;
 using Multiblog.Core.Repository;
 using Multiblog.Model;
 
